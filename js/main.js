cc72f7e8440b9ab6d92237c883a6fb063ab80ec3
Ilya Voytov <ilya@voytov.com>
2023-10-30 20:09:20 -0400

    get all the transaction data into grid

--- js/main.js
+++ js/main.js
@@ -1,34 +1,57 @@
 const bronxUrl = "transactions/bronx.csv"
 
 Papa.parse(bronxUrl, {
-	download: true,
-	complete: function(results) {
-		console.log(results);
-	}
+  download: true,
+  complete: function (results) {
+    console.log(results);
+  }
 });
 
+// Function to load CSV file using PapaParse
+function loadCSV(url) {
+  return new Promise((resolve, reject) => {
+    Papa.parse(url, {
+      download: true,
+      header: true,
+      skipEmptyLines: true,
+      complete: results => {
+        resolve(results.data);
+      },
+      error: error => {
+        reject(error.message);
+      }
+    });
+  });
+}
 
-const columnDefs = [
-    { field: "make" },
-    { field: "model" },
-    { field: "price" }
-  ];
-  
-  // specify the data
-  const rowData = [
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxster", price: 72000 }
-  ];
-  
-  // let the grid know which columns and what data to use
-  const gridOptions = {
-    columnDefs: columnDefs,
-    rowData: rowData
-  };
-  
-  // setup the grid after the page has finished loading
-  document.addEventListener('DOMContentLoaded', () => {
+// URLs of the CSV files you want to load
+const csvUrls = [
+  'manhattan.csv',
+  'bronx.csv',
+  'brooklyn.csv',
+  'queens.csv',
+  'statenisland.csv'
+];
+
+// Array to store promises for each CSV file
+const csvPromises = csvUrls.map(url => loadCSV(`transactions/${url}`));
+
+// Use Promise.all to wait for all promises to resolve
+Promise.all(csvPromises)
+  .then(resultsArray => {
+    // Combine the results from all CSV files
+    const combinedData = resultsArray.reduce((acc, data) => acc.concat(data), []);
+
+    // Initialize AG Grid
+    const gridOptions = {
+      columnDefs: Object.keys(combinedData[0]).map(key => ({ headerName: key, field: key })),
+      rowData: combinedData
+    };
+
+    // Create AG Grid
     const gridDiv = document.querySelector('#myGrid');
     new agGrid.Grid(gridDiv, gridOptions);
+  })
+  .catch(error => {
+    console.error('Error loading CSV files:', error);
   });
