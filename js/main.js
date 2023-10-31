0788a7078067db681a46dd9a93901cb88112d1a7
Ilya Voytov <ilya@voytov.com>
2023-10-31 02:09:10 -0400

    Grid filters, dates, transaction proceeds

--- js/main.js
+++ js/main.js
@@ -5,7 +5,22 @@ function loadCSV(url) {
       download: true,
       header: true,
       skipEmptyLines: true,
+      dynamicTyping: true,
       complete: results => {
+        // Convert "SALE DATE" property to JavaScript Date objects
+          results.data = results.data.map(obj => {
+          // Use destructuring to get other properties if needed
+          const { "SALE DATE": saleDate, ...rest } = obj;
+
+          // Convert string to Date, set to midnight (otherwise date filter doesn't work)
+          const saleDateObj = new Date(saleDate);
+          saleDateObj.setHours(0,0,0,0)
+
+          // Add other properties back if needed
+          return { "SALE DATE": saleDateObj, ...rest };
+        });
+
+
         resolve(results.data);
       },
       error: error => {
@@ -27,16 +42,40 @@ const csvUrls = [
 // Array to store promises for each CSV file
 const csvPromises = csvUrls.map(url => loadCSV(`transactions/${url}`));
 
+const defaultColDef = {
+  flex: 1,
+  minWidth: 150,
+  filter: 'agTextColumnFilter',
+  menuTabs: ['filterMenuTab'],
+  autoHeaderHeight: true,
+  wrapHeaderText: true
+}
+
 // Use Promise.all to wait for all promises to resolve
 Promise.all(csvPromises)
   .then(resultsArray => {
     // Combine the results from all CSV files
     const combinedData = resultsArray.reduce((acc, data) => acc.concat(data), []);
 
+    const columnDefs = Object.keys(combinedData[0]).map(key => ({ headerName: key, field: key }))
+    
+    columnDefs[0].filter = 'agDateColumnFilter'
+
+    const formattedCurrency = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    });
+
+    columnDefs[columnDefs.length - 1].valueFormatter = (params) => formattedCurrency.format(params.value)
+    columnDefs[columnDefs.length - 1].filter = 'agNumberColumnFilter'
+
     // Initialize AG Grid
     const gridOptions = {
-      columnDefs: Object.keys(combinedData[0]).map(key => ({ headerName: key, field: key })),
-      rowData: combinedData
+      columnDefs: columnDefs,
+      rowData: combinedData,
+      defaultColDef: defaultColDef,
     };
 
     // Create AG Grid
