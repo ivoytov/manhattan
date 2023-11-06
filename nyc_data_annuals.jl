1a04fc8d5b18f08819f6b36991502fe5f478868c
Ilya Voytov <ilya@voytov.com>
2023-11-06 00:36:21 -0500

    Index generation

--- nyc_data_annuals.jl
+++ nyc_data_annuals.jl
@@ -1,5 +1,5 @@
-using DataFrames
-using XLSX, ExcelReaders
+using DataFrames, CategoricalArrays
+using XLSX, ExcelReaders, Dates
 
 # Define URL prefixes
 url_prefix = "https://www.nyc.gov/assets/finance/downloads/pdf/rolling_sales"
@@ -25,46 +25,24 @@ function get_file(borough, year)
     link = "$url_prefix/annualized-sales/$year/$(year)_$borough.$ext"
 
     filename = download(link)
-    data = missing
-    if ext == "xls"
-        xf = openxl(filename)
-        sheet_name = xf.workbook.sheet_names()[1]
-        data = DataFrame(readxlsheet(filename, sheet_name; skipstartrows=5), cols)
-    else
-
-        data = XLSX.openxlsx(filename) do xf
-            sheet_name = XLSX.sheetnames(xf)[1]
-            DataFrame(XLSX.gettable(
-                xf[sheet_name], "A:U"; 
-                header=false, 
-                column_labels=cols, 
-                first_row=(year < 2020 ? 6 : 9), 
-                infer_eltypes=true
-                )
+    data = XLSX.openxlsx(filename) do xf
+        sheet_name = XLSX.sheetnames(xf)[1]
+        DataFrame(XLSX.gettable(
+            xf[sheet_name], "A:U"; 
+            header=false, 
+            column_labels=cols, 
+            first_row=(year < 2020 ? 6 : 9), 
+            infer_eltypes=true
             )
-        end
+        )
     end
     println("$borough $year $(maximum(data[:, "SALE DATE"]))")
 
     return data
 end
 
-function get_legacy_file(borough)
-    links = [
-        "$url_prefix_pre_2021/annualized-sales/2009_$borough.xls",
-        "https://www1.nyc.gov/assets/finance/downloads/pdf/09pdf/rolling_sales/sales_2008_$borough.xls",
-        "https://www1.nyc.gov/assets/finance/downloads/excel/rolling_sales/sales_2007_$borough.xls",
-        ["https://www1.nyc.gov/assets/finance/downloads/sales_$(borough != "statenisland" ? borough : "si")_0$n.xls" for n in 3:6]...
-    ]
-
-    return vcat([ExcelFiles.read(link, skip=4, column_labels=cols, dateformat="yyy-mm-dd") for link in links]...)
-end
-
 # Concatenate DataFrames
 new_years = vcat([get_file(borough, year) for borough in boroughs, year in 2018:2022]...)
 CSV.write("transactions/transactions_2018-2022.csv", new_years)
 
-xls_years = vcat([get_file(borough, year) for borough in boroughs, year in 2010:2018]...)
-
-# old_years = vcat([get_legacy_file(borough) for borough in boroughs]...)
 
---
+++ repeat_sales_index.jl
@@ -0,0 +1,100 @@
+using DataFrames, Dates, CSV, GLM
+
+df = CSV.File("transactions/nyc_2018-2022.csv") |> DataFrame
+boroughs = ["manhattan", "bronx", "brooklyn", "queens", "statenisland"]
+rolling_sales = vcat([CSV.read("transactions/$borough.csv", DataFrame) for borough in boroughs]...)
+rolling_sales = rolling_sales[rolling_sales[!, "SALE DATE"] .> Date(2022,12,31), :]
+df = vcat(df, rolling_sales)
+
+borough_dict = Dict(1 => "Manhattan", 2 => "Bronx", 3 => "Brooklyn", 4 => "Queens", 5 => "Staten Island")
+df[!, "BOROUGH"] = map(borough_id -> borough_dict[borough_id], df[!, "BOROUGH"])
+
+df = df[df[!, "SALE PRICE"] .> 100000, :]
+
+SFH_CATEGORIES = r"01"
+COOP_CATEGORIES = r"09|[^-]10|17"
+CONDO_CATEGORIES = r"12|13"
+
+sfh = occursin.(SFH_CATEGORIES, df[!, "BUILDING CLASS CATEGORY"])
+coops = occursin.(COOP_CATEGORIES, df[!, "BUILDING CLASS CATEGORY"])
+condos = occursin.(CONDO_CATEGORIES, df[!, "BUILDING CLASS CATEGORY"])
+
+df[!, :house_class] .= "Condo"
+df[coops, :house_class] .= "Coop"
+df[sfh, :house_class] .= "SFH"
+
+df = df[coops .| condos .| sfh, :]
+
+# Create uid for condos and sfh
+condo_sfh_filter = condos .| sfh
+df[!, :uid] .= string.(df[!, :BLOCK]) .* '_' .* string.(df[!, :LOT])
+
+# Create uid for coops
+coops_filter = df[!, :house_class] .== "Coop"
+apartment = split.(df[coops_filter, "ADDRESS"],", "; limit=2) .|> x -> get(x,2,"") .|> lowercase .|> x -> replace(x, r"(?:UNIT)?(?:APT)?[^A-Z0-9\n]" => "")
+missing_apartment = map(x-> x=="",apartment)
+
+df[coops_filter, :uid] .= string.(df[coops_filter, :BLOCK]) .* '_' .* apartment
+rows_to_remove = findall(coops_filter)[missing_apartment]
+df = df[setdiff(1:end,rows_to_remove), :]
+
+df = rename(df, "BOROUGH" => :borough, "SALE DATE" => :sale_date, "SALE PRICE" => :sale_price)
+
+cols = [:borough, :sale_date, :uid, :sale_price]
+archive = CSV.read("transactions/nyc_real_estate_211231.csv", DataFrame)[!, cols]
+archive = dropmissing(archive, [:uid])
+archive = archive[archive.sale_date .< Date(2018,1,1), :]
+
+
+df = vcat(df[:, cols], archive[:, cols])
+
+# Convert 'sale_date' to PeriodIndex with frequency
+df[!, :period] = Dates.lastdayofmonth.(df.sale_date)
+
+# Drop duplicates based on 'period' and 'uid', keeping the last one
+df = dropmissing(df, [:period, :uid])
+
+df =  df[df.sale_price .> 1.0e5, :] |> df -> sort(df, [:uid, :sale_date])
+grouped = groupby(df, ["borough", "uid"])
+grouped = grouped[combine(grouped, nrow).nrow .> 1]
+
+rng = sort(unique(df.period))
+
+n = sum(combine(grouped, gp -> nrow(gp) - 1).x1)
+p = size(rng, 1)  # degrees of freedom aka num of periods
+X = zeros(Int64, (n, p))   # first column of X will become Y vector
+Y = zeros(Float64, (n, 1))
+Δ = zeros(Int64, (n , 1))
+row = 1
+
+for group in grouped
+    for i in 1:size(group,1)-1
+        buy = group[i, :]
+        sell = group[i+1, :]
+        q₁ = findfirst(rng .== buy.period)
+        q₂ = findfirst(rng .== sell.period)
+        X[row, q₁] = -1
+        X[row, q₂] = 1
+        Y[row] = log(sell.sale_price) - log(buy.sale_price) 
+        Δ[row] = q₂ - q₁
+        global row += 1
+    end
+end
+
+x = DataFrame(X, Symbol.(rng))
+x[!, :Y] = Y[:]
+formula = term(:Y) ~ term(0) + sum(term.(Symbol.(rng)))
+
+lm_model = lm(formula, x)
+e = residuals(lm_model)
+
+R = DataFrame(hcat(Δ, e), [:Δ, :e])
+w_model = lm(@formula(e^2 ~ 0 + Δ), R)
+
+w = 1 ./ sqrt.(predict(w_model))
+
+w[isinf.(w)] .= 1
+wrs_model = lm(formula, x, wts=w)
+
+idx = DataFrame(period = rng, home_price_index = 100 * ℯ .^ coef(wrs_model))
+CSV.write("home_price_index.csv", idx)
