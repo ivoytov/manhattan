485c65651f2f812ae5470ad7bdd9e353885f4d22
Ilya Voytov <ilya@voytov.com>
2023-11-19 03:49:32 -0500

    Refactoring index calculation code

--- repeat_sales_index.jl
+++ repeat_sales_index.jl
@@ -1,56 +1,79 @@
 using DataFrames, Dates, CSV, GLM
 
-function calc_index(df)
-    grouped = groupby(df, [:borough, :uid])
-
-    rng = sort(unique(df.period))
-
-    n = sum(combine(grouped, gp -> nrow(gp) - 1).x1)
-    p = size(rng, 1)  # degrees of freedom aka num of periods
-    X = zeros(Int64, (n, p))
-    Y = zeros(Float64, (n, 1))
-    Δ = zeros(Int64, (n, 1))
-    row = 1
-
-    # in case of blank matrix, return empty frame
-    if n == 0
-        return DataFrame()
-    end
-
+# Function for creating the matrix for the regression model
+function create_regression_matrix(grouped, periods)
+    total_transactions = sum(nrow(group) - 1 for group in grouped)
+    num_periods = length(periods)
+    
+    # Initialize matrices
+    X = zeros(Int64, total_transactions, num_periods)
+    Y = zeros(Float64, total_transactions)
+    delta_days = zeros(Int64, total_transactions)
+    
+    # Fill the matrices
+    row_index = 1
     for group in grouped
-        for i in 1:size(group, 1)-1
+        for i in 1:nrow(group) - 1
             buy = group[i, :]
-            sell = group[i+1, :]
-            q₁ = findfirst(rng .== buy.period)
-            q₂ = findfirst(rng .== sell.period)
-            X[row, q₁] = -1
-            X[row, q₂] = 1
-            Y[row] = log(sell.sale_price) - log(buy.sale_price)
-            Δ[row] = length(buy.period:Month(1):sell.period)
-
-            row += 1
+            sell = group[i + 1, :]
+            q1_index = findfirst(==(buy.period), periods)
+            q2_index = findfirst(==(sell.period), periods)
+            X[row_index, q1_index] = -1
+            X[row_index, q2_index] = 1
+            Y[row_index] = log(sell.sale_price) - log(buy.sale_price)
+            delta_days[row_index] = Dates.days(sell.period - buy.period)
+            row_index += 1
         end
     end
+    return X, Y, delta_days
+end
 
-    x = DataFrame(X, Symbol.(rng))
-    x[!, :Y] = Y[:]
-    formula = term(:Y) ~ term(0) + sum(term.(Symbol.(rng)))
+# Function for computing weighted regression
+function compute_weighted_regression(X, Y, delta_days)
 
-    lm_model = lm(formula, x)
-    e = residuals(lm_model)
+    data_frame = DataFrame(X, Symbol.("Q" .* string.(1:size(X, 2))))
+    data_frame.Y = Y
+    formula = term(:Y) ~ term(0) + sum([term("Q$i") for i in 1:size(X, 2)])
 
-    R = DataFrame(hcat(Δ, e), [:Δ, :e])
-    w_model = lm(@formula(e^2 ~ 0 + Δ), R)
+    # Least squares regression
+    lm_model = lm(formula, data_frame)
+    residuals_squared = residuals(lm_model) .^ 2
+    
+    # Weighted model
+    R = DataFrame(Δ = delta_days, e_squared = residuals_squared)
+    w_model = lm(@formula(e_squared ~ 0 + Δ), R)
+    weights = 1 ./ sqrt.(predict(w_model))
+    replace!(weights, Inf=>1)
+    
+    # Weighted least squares regression
+    wrs_model = lm(formula, data_frame, wts=weights)
+    return wrs_model
+end
 
-    w = 1 ./ sqrt.(predict(w_model))
+# Calculate the home price index
+function calculate_home_price_index(df)
+    grouped = groupby(df, [:borough, :uid])
 
-    w[isinf.(w)] .= 1
-    wrs_model = lm(formula, x, wts=w)
-
-    idx = DataFrame(period=rng, home_price_index=100 * ℯ .^ coef(wrs_model))
-    idx.home_price_index = 100 * idx.home_price_index ./ idx.home_price_index[1]
-    idx[:, Not(:period)] = round.(idx[:, Not(:period)], digits=2)
-    idx
+    if isempty(grouped)
+        return DataFrame()
+    end
+    
+    # Get unique periods and sort them
+    periods = sort(unique(df.period))
+    
+    # Prepare data for regression
+    X, Y, delta_days = create_regression_matrix(grouped, periods)
+    if isempty(Y)
+        return DataFrame()
+    end
+    
+    # Weighted regression
+    wrs_model = compute_weighted_regression(X, Y, delta_days)
+    
+    # Prepare the index DataFrame
+    index_values = coef(wrs_model)
+    base_index = index_values[1]
+    DataFrame(period = periods, home_price_index = round.(100 * exp.(index_values) / exp(base_index)), digits=1)
 end
 
 # Function to read CSV file into DataFrame
@@ -62,7 +85,7 @@ df = read_csv("transactions/nyc_2018-2022.csv")
 # Consolidate rolling sales data for each borough and filter for sales after the specified date
 boroughs = ["manhattan", "bronx", "brooklyn", "queens", "statenisland"]
 rolling_sales = reduce(vcat, [read_csv("transactions/$borough.csv") for borough in boroughs])
-rolling_sales = filter(row -> row["SALE DATE"] > Date(2022, 12, 31), rolling_sales)
+rolling_sales = filter(["SALE DATE"] => >(Date(2022, 12, 31)), rolling_sales)
 
 # Combine annuals and rolling data sets
 df = vcat(df, rolling_sales)
@@ -95,12 +118,12 @@ end
 df.house_class = map(house_class_map, df.house_class)
 
 # Filter for specific house classes
-filter!(row -> row.house_class ∈ ["Coop", "Condo", "SFH"], df)
+filter!(:house_class => ∈(["Coop", "Condo", "SFH"]), df)
 
 # Read the archive and filter
 cols = [:borough, :sale_date, :sale_price, :house_class, :neighborhood, :address, :block, :lot]
 archive = read_csv("transactions/nyc_real_estate_211231.csv")[!, cols]
-filter!(row -> row.sale_date < Date(2018, 1, 1), archive)
+filter!(:sale_date => <(Date(2018, 1, 1)), archive)
 
 # Concatenate df and archive keeping only the selected columns
 df = vcat(df[:, cols], archive)
@@ -125,7 +148,7 @@ df.period = Dates.lastdayofmonth.(df.sale_date)
 
 # Remove duplicates and filter based on sale price
 unique!(df, [:period, :uid])
-filter!(row -> 2.5e5 < row.sale_price < 1.5e7, df)
+filter!(:sale_price => price -> 2.5e5 < price < 1.5e7, df)
 sort!(df, [:sale_date])
 
 # Filter for reasonable transaction frequencies
@@ -151,21 +174,17 @@ outliers = (grouped_df
 CSV.write("transactions/outliers.csv", outliers)
 
 # Remove outliers from the main dataframe
-df = filter(gp -> !is_outlier(gp), grouped_df) |> gdf -> combine(gdf, names(df)...)
+df = filter(!is_outlier, grouped_df) |> gdf -> combine(gdf, names(df)...)
 
 # Calculating and exporting home price indices
-home_price_index = calc_index(df)
+home_price_index = calculate_home_price_index(df)
 home_price_subindex = combine(groupby(df, [:borough, :house_class])) do sdf
-    calc_index(sdf)
+    calculate_home_price_index(sdf)
 end
 home_price_neighborhoods = combine(groupby(df, [:borough, :neighborhood])) do sdf
-    calc_index(sdf)
+    calculate_home_price_index(sdf)
 end
 
 CSV.write("home_price_index.csv", home_price_index)
 CSV.write("home_price_subindex.csv", home_price_subindex)
 CSV.write("home_price_neighborhoods.csv", home_price_neighborhoods)
-
-
-
-
