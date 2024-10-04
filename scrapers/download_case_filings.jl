0dd5cbd65f0f5d79c367decd7b74f0b47ae10616
Ilya Voytov <ilya@voytov.com>
2024-10-04 01:26:02 -0400

    automagically start progress

--- scrapers/download_case_filings.jl
+++ scrapers/download_case_filings.jl
@@ -4,7 +4,7 @@ using CSV, DataFrames, ProgressMeter, Base.Threads, Dates, Random
 # Get filings
 function get_filings()
     rows = get_data()
-    process_data(rows, 4, "--progress" in ARGS)
+    process_data(rows, 4, !isempty(get(ENV, "WSS", "")))
 end
 
 # Define the FilingType as a constant dictionary
@@ -58,18 +58,6 @@ function get_data()
     filter!(row -> !(row.case_number in not_in_cef), rows)
     
     sort!(rows, order(:auction_date), rev=true)
-
-    start_dt = Dates.today() - Dates.Day(0)
-    end_dt = Dates.today() + Dates.Day(14)
-    urgent_cases = filter(rows) do row
-        is_soon = start_dt < row.auction_date < end_dt 
-        filename = get_filename(row.case_number)
-        has_notice_of_sale = isfile("saledocs/noticeofsale/$filename")
-        return is_soon && !has_notice_of_sale
-    end
-    for case in eachrow(urgent_cases)
-        println("$(case.case_number) $(case.borough) $(case.auction_date)")
-    end
 
     transform!(rows, [:case_number, :auction_date] => ByRow(missing_filings) => :missing_filings)
     filter!(row -> !isempty(row.missing_filings), rows)
