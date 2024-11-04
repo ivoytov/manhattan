363273ed0e438854f3e46db70aabed4f0f693c3b
Ilya Voytov <ilya@voytov.com>
2024-11-03 22:37:56 -0500

    try downgrading ubuntu

--- process_auctions.jl
+++ process_auctions.jl
@@ -1,10 +1,12 @@
-using DataFrames, Dates, CSV, GLM, Statistics, GeoJSON, JSON3
+using DataFrames, Dates, CSV, GLM, Statistics, GeoJSON, JSON3, HTTP
 
 
 # Function to read CSV file into DataFrame
 read_csv(file) = CSV.read(file, DataFrame)
 
 borough_dict = Dict(1 => "Manhattan", 2 => "Bronx", 3 => "Brooklyn", 4 => "Queens", 5 => "Staten Island")
+borough_id_dict = Dict("Manhattan" => "1", "Bronx"=>"2", "Brooklyn"=>"3", "Queens" =>"4", "Staten Island"=>"5")
+
 
 # Initialize and preprocess datasets
 function initialize_data()
@@ -38,6 +40,42 @@ function main()
 
 end
 
+
+function condo_base_bbl_key(borough, block, lot)
+    outfields = "CONDO_BASE_BBL_KEY"
+    url = "https://services6.arcgis.com/yG5s3afENB5iO9fj/arcgis/rest/services/DTM_ETL_DAILY_view/FeatureServer/4"
+    # query = "Borough = 'MX' and Block = 459 and Lot = 1113"
+    query = "UNIT_BORO = '$(borough_id_dict[borough])' and UNIT_BLOCK=$block and UNIT_LOT=$lot"
+    @show query
+    result = esri_query(url, outfields, query)
+    return result[1]["attributes"]["CONDO_BASE_BBL_KEY"]
+end
+
+function esri_query(url, outfields, query)
+    params = Dict("f"=>"JSON", "outfields"=>outfields, "where"=>query)
+    r = HTTP.request("POST", "$(url)/query",
+                 ["Content-Type" => "application/x-www-form-urlencoded", "accept"=>"application/json"],
+                 HTTP.URIs.escapeuri(params))
+    json = JSON3.read(String(r.body))
+    return json.features
+end
+
+function condo_billing_bbl(condo_base_bbl_key)
+    outfields = "CONDO_BILLING_BBL"
+    url = "https://services6.arcgis.com/yG5s3afENB5iO9fj/arcgis/rest/services/DTM_ETL_DAILY_view/FeatureServer/3"
+    # query = "Borough = 'MX' and Block = 459 and Lot = 1113"
+    query = "CONDO_BASE_BBL_KEY = $condo_base_bbl_key"
+    result = esri_query(url, outfields, query) 
+    return result[1]["attributes"]["CONDO_BILLING_BBL"]
+end
+
+function pluto(bbl)
+    outfields = "*"
+    url = "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/ArcGIS/rest/services/MAPPLUTO/FeatureServer/0"
+    query = "BBL = $bbl"
+    result = esri_query(url, outfields, query) 
+    return result[1]["attributes"]
+end
 main()
 
 
